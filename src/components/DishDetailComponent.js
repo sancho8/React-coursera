dddbc51f4ba41ab419dcc9f5ea0341b0a756ba8d
sancho8 <98sancho@ukr.net>
2021-01-18 00:30:27 +0200

    Redux Thunk

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -17,6 +17,7 @@ import {
   ModalHeader,
   Row,
 } from "reactstrap";
+import { Loading } from "./LoadingComponent";
 import { Control, LocalForm, Errors } from "react-redux-form";
 
 function RenderDish({ dish }) {
@@ -196,7 +197,23 @@ class CommentForm extends Component {
 }
 
 const DishDetail = (props) => {
-  if (props.dish)
+  if (props.isLoading) {
+    return (
+      <div className="container">
+        <div className="row">
+          <Loading />
+        </div>
+      </div>
+    );
+  } else if (props.errMess) {
+    return (
+      <div className="container">
+        <div className="row">
+          <h4>{props.errMess}</h4>
+        </div>
+      </div>
+    );
+  } else if (props.dish != null) {
     return (
       <div className="container">
         <div className="row">
@@ -225,7 +242,7 @@ const DishDetail = (props) => {
         </div>
       </div>
     );
-  else return <div className="row"></div>;
+  } else return <div className="row"></div>;
 };
 
 export default DishDetail;
